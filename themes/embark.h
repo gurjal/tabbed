275442569fa3ed41766a1091e1284b6337f733b6
gurjal <shaanraya135@gmail.com>
2022-07-07 19:06:39 -0700

    default configs and themes directory set

---
+++ themes/embark.h
@@ -0,0 +1,9 @@
+/*
+* tabbed/themes/embark.h
+*
+*/
+
+static const char* normbgcolor  = "#1E1C31";
+static const char* normfgcolor  = "#A6B3CC";
+static const char* selbgcolor   = "#1E1C31";
+static const char* selfgcolor   = "#F8F8F2";
