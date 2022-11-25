adfe61ba8890347b1426a639d0ffd4c71051c889
gurjal <shaanraya135@gmail.com>
2022-11-24 20:44:53 -0800

    new theme tundra

---
+++ themes/tundra.h
@@ -0,0 +1,9 @@
+/*
+ * tabbed/themes/tundra.h
+ *
+ */
+
+static const char* normbgcolor  = "#111827";
+static const char* normfgcolor  = "#6B7280";
+static const char* selbgcolor   = "#111827";
+static const char* selfgcolor   = "#D1D5DB";
