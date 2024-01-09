50c1e3849396395c411aca180c49e4b915ea48d7
gurjal <gurjal@proton.me>
2024-01-08 20:14:18 -0800

    theme edit

--- themes/seoul256.h
+++ themes/seoul256.h
@@ -3,7 +3,11 @@
 *
 */
 
-static const char* normbgcolor  = "#3a3a3a";
+// for darker seoul256 bg
+/* static const char* normbgcolor  = "#3a3a3a"; */
+/* static const char* selbgcolor   = "#3a3a3a"; */
+
+static const char* normbgcolor  = "#4B4B4B";
 static const char* normfgcolor  = "#949494";
-static const char* selbgcolor   = "#3a3a3a";
+static const char* selbgcolor   = "#4B4B4B";
 static const char* selfgcolor   = "#d0d0d0";
