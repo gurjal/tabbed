e4c808c36eea171e8b0254de6c859384296b4c98
gurjal <gurjal@proton.me>
2024-03-14 06:15:05 -0700

    ayu theme changes

--- themes/ayu.h
+++ themes/ayu.h
@@ -4,6 +4,8 @@
 */
 
 static const char* normfgcolor  = "#3e4b59";
-static const char* normbgcolor  = "#0f1419";
+static const char* normbgcolor  = "#0d1017";
+/* static const char* normbgcolor  = "#0f1419"; */
 static const char* selfgcolor   = "#e6e1cf";
-static const char* selbgcolor   = "#0f1419";
+static const char* selbgcolor   = "#0d1017";
+/* static const char* selbgcolor   = "#0f1419"; */
